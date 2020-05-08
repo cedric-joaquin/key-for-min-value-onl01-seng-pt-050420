5aa999561ba3659f7b2d605a9d36ea01833a334f
Cedric Joaquin <joaquin.cedric@gmail.com>
2020-05-08 18:24:13 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |item, price| 
     if min == nil || price < min 
       price = min
+      lowest_item = item 
     end 
-  
-  
+  lowest_item 
 end
